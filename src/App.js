8bd39aa36ad1d53d2caa3639d7809bfacfc4805e
Kirill Golygin <kirik3007@gmail.com>
2023-05-19 12:34:26 +0300

    work with array

--- src/App.js
+++ src/App.js
@@ -4,23 +4,30 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [
+        { name: 'Linda', id: '1dsfe12' },
+        { name: 'Frank', id: '1dsasdd2' },
+        { name: 'Jacky', id: '1dshgj54' },
+        { name: 'Andrei', id: '1dsh1wsa4' }
+      ]
+    };
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {
+          this.state.monsters.map((monster) => {
+            return (
+              <div key={monster.id}>
+                <h1>{monster.name}</h1>
+              </div>);
+          })
+        }
       </div>
     );
   }
