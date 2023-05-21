e9adc2bb4ccc2a4afb95e79fa2b53743f7c6c65b
Kirill Golygin <kirik3007@gmail.com>
2023-05-21 18:14:48 +0300

    migrate project to functional components

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,95 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 
 import CardList from './components/card-list/card-list.component';
 import SearchBox from './components/search-box/search-box.component';
 import './App.css';
 
-class App extends Component {
+const App = () => {
+  const [searchField, setSearchField] = useState('');
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMosters, setFilteredMonsters] = useState(monsters);
 
-  constructor() {
-    super();
-
-    this.state = {
-      monsters: [],
-      searchField: '',
+  useEffect(() => {
+    const getMonsters = async () => {
+      const res = await fetch('https://jsonplaceholder.typicode.com/users');
+      if (res.ok) {
+        const users = await res.json();
+        setMonsters(users);
+      } else {
+        alert('http error:' + res.status);
+      }
     };
-  }
 
-  async componentDidMount() {
-    const res = await fetch('https://jsonplaceholder.typicode.com/users');
-    if (res.ok) {
-      const users = await res.json();
-      this.setState({ monsters: users });
-    } else {
-      alert('http error:' + res.status);
-    }
-  }
+    getMonsters();
+  }, []);
 
-  onSearchChange = (e) => {
-    const searchField = e.target.value.toLocaleLowerCase();
-    this.setState({ searchField });
-  }
+  useEffect(() => {
+    const newFilteredMosters = monsters.filter((monster) => monster.name.toLocaleLowerCase().includes(searchField));
+    setFilteredMonsters(newFilteredMosters);
+  }, [monsters, searchField]);
 
-  render() {
-    const { monsters, searchField } = this.state;
-    const { onSearchChange } = this;
+  const onSearchChange = (e) => {
+    const searchFieldString = e.target.value.toLocaleLowerCase();
+    setSearchField(searchFieldString);
+  };
 
-    const filteredMosters = monsters.filter((monster) => monster.name.toLocaleLowerCase().includes(searchField));
-
-    return (
-      <div className="App">
-        <h1 className='app-title'>Monsters Rolodex</h1>
-        <SearchBox
-          onChangeHandler={onSearchChange}
-          placeholder={'search monsters'}
-          className={'search-box__monsters'}
-        />
-        <CardList monsters={filteredMosters} />
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <h1 className='app-title'>Monsters Rolodex</h1>
+      <SearchBox
+        onChangeHandler={onSearchChange}
+        placeholder={'search monsters'}
+        className={'search-box__monsters'}
+      />
+      <CardList monsters={filteredMosters} />
+    </div>
+  )
 }
+
+// class App extends Component {
+
+//   constructor() {
+//     super();
+
+//     this.state = {
+//       monsters: [],
+//       searchField: '',
+//     };
+//   }
+
+// async componentDidMount() {
+//   const res = await fetch('https://jsonplaceholder.typicode.com/users');
+//   if (res.ok) {
+//     const users = await res.json();
+//     this.setState({ monsters: users });
+//   } else {
+//     alert('http error:' + res.status);
+//   }
+// }
+
+//   onSearchChange = (e) => {
+//     const searchField = e.target.value.toLocaleLowerCase();
+//     this.setState({ searchField });
+//   }
+
+//   render() {
+//     const { monsters, searchField } = this.state;
+//     const { onSearchChange } = this;
+
+//     const filteredMosters = monsters.filter((monster) => monster.name.toLocaleLowerCase().includes(searchField));
+
+//     return (
+//       <div className="App">
+//         <h1 className='app-title'>Monsters Rolodex</h1>
+//         <SearchBox
+//           onChangeHandler={onSearchChange}
+//           placeholder={'search monsters'}
+//           className={'search-box__monsters'}
+//         />
+//         <CardList monsters={filteredMosters} />
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,18 +1,15 @@
-import { Component } from "react";
+import { useState } from "react";
 import MonsterCard from '../monster-card/monster-card.component';
 import './card-list.styles.css';
 
-class CardList extends Component {
-    render() {
-        const { monsters } = this.props;
-        return (
-            <div className="cards-list">
-                {monsters.map(monster => {
-                    return <MonsterCard monster={monster} key={monster.id} />
-                })}
-            </div>
-        )
-    }
-}
+const CardList = ({ monsters }) => (
+    <div className="cards-list">
+        {monsters.map(monster => {
+            return <MonsterCard monster={monster} key={monster.id} />
+        })}
+    </div>
+)
+
+
 
 export default CardList;
